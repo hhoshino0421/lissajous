b6162d33853f4b3ed4636412e6ff5f282eee3137
Hoshino Hitoshi <hhoshino-main@memoad.jp>
2017-03-27 18:39:20 +0900

    インデントを8から4に変更。演算子の位置を調整。

--- Lissajous.go
+++ Lissajous.go
@@ -32,13 +32,13 @@ func lissajous(out io.Writer) {
 		delay	= 10		//10ms単位でのフレーム間遅延
 	)
 
-	freq := rand.Float64() * 3.0		//発振器yの相対周波数
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0				//位相差
+	freq 	:= rand.Float64() * 3.0		//発振器yの相対周波数
+	anim 	:= gif.GIF{LoopCount: nframes}
+	phase 	:= 0.0				//位相差
 
 	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0,0,2*size+1,2*size+1)
-		img := image.NewPaletted(rect,palette)
+		rect 	:= image.Rect(0,0,2*size+1,2*size+1)
+		img 	:= image.NewPaletted(rect,palette)
 
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
@@ -46,9 +46,9 @@ func lissajous(out io.Writer) {
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
 		}
 
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
+		phase 		+= 0.1
+		anim.Delay 	= append(anim.Delay, delay)
+		anim.Image 	= append(anim.Image, img)
 
 	}
 
